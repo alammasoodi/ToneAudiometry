edb1a1456989a8bb882489d13b097aab1ac5276a
alammasoodi <alam@trialx.com>
2017-12-19 12:54:45 +0530

    tone generator method added

--- app/src/main/java/com/example/alam/toneaudiometrytest/ToneAudiometryFragment.java
+++ app/src/main/java/com/example/alam/toneaudiometrytest/ToneAudiometryFragment.java
@@ -3,12 +3,15 @@ package com.example.alam.toneaudiometrytest;
 import android.app.Fragment;
 import android.content.Context;
 import android.content.res.AssetFileDescriptor;
+import android.media.AudioFormat;
 import android.media.AudioManager;
+import android.media.AudioTrack;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -19,6 +22,7 @@ import java.io.IOException;
 
 public class ToneAudiometryFragment extends Fragment {
     View v;
+    Button leftEarButton,rightEarButton;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.fragment_audiometry, container, false);
@@ -29,16 +33,17 @@ public class ToneAudiometryFragment extends Fragment {
             AudioObj.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
                 @Override
                 public void onPrepared(final MediaPlayer mediaPlayer) {
-
-                    mediaPlayer.setVolume(0, 0.1f);
-                    mediaPlayer.start();
+                    AudioTrack tone = generateTone(200, 5000);
+                    tone.play();
+//                    mediaPlayer.setVolume(0, 0.1f);
+//                    mediaPlayer.start();
 
                 }
             });
 
             AudioObj.setAudioStreamType(AudioManager.STREAM_MUSIC);
             try {
-                AssetFileDescriptor afd = getActivity().getAssets().openFd("test2.mp3");
+                AssetFileDescriptor afd = getActivity().getAssets().openFd("beep-11.mp3");
                 AudioObj.setDataSource(afd.getFileDescriptor());
             }catch (IOException e){}
             AudioObj.prepareAsync();
@@ -50,5 +55,20 @@ public class ToneAudiometryFragment extends Fragment {
     }
     public void handleHeadphonesState(Context context){
 
+    }
+    private AudioTrack generateTone(double freqHz, int durationMs)
+    {
+        int count = (int)(44100.0 * 2.0 * (durationMs / 1000.0)) & ~1;
+        short[] samples = new short[count];
+        for(int i = 0; i < count; i += 2){
+            short sample = (short)(Math.sin(2 * Math.PI * i / (44100.0 / freqHz)) * 0x7FFF);
+            samples[i + 0] = sample;
+            samples[i + 1] = sample;
+        }
+        AudioTrack track = new AudioTrack(AudioManager.STREAM_MUSIC, 44100,
+                AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT,
+                count * (Short.SIZE / 8), AudioTrack.MODE_STATIC);
+        track.write(samples, 0, count);
+        return track;
     }
     }
